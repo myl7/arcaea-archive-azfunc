a7769db120ae9d624b0fc6e7d9cc98a399c22524
myl7 <myl.ustc@gmail.com>
2020-12-07 06:28:52 +0800

    Finish api. Use cosmos table api to store.

---
+++ ArcaeaProberService.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace arcaea_archive_azfunc
+{
+    public static class ArcaeaProberService
+    {
+        private const string ArcaeaProberUrl = "wss://arc.estertion.win:616";
+        private const string Query = "984569312 7 11";
+
+        public static async Task<List<string>> Get()
+        {
+            var url = new Uri(ArcaeaProberUrl);
+            using var ws = new ClientWebSocket();
+            await ws.ConnectAsync(url, CancellationToken.None);
+
+            var util = new WebSocketUtils(ws);
+            await util.SendTextAsync(Query);
+            var queriedReply = Encoding.ASCII.GetString(await util.ReceiveMsgAsync(WebSocketMessageType.Text));
+            if (queriedReply != "queried")
+            {
+                throw new ArchiveException(
+                    $"ArcaeaProberGet fail: Query replies {queriedReply} other than queried");
+            }
+
+            var decUrl = Environment.GetEnvironmentVariable("BROTLI_DEC_AZFUNC_URL");
+            var results = new List<string>();
+            var tasks = new List<Task>();
+
+            async Task BrotliDec(byte[] data, int i)
+            {
+                var req = WebRequest.Create(decUrl!);
+                req.Method = WebRequestMethods.Http.Post;
+                req.ContentType = "application/octet-stream";
+                req.ContentLength = data.Length;
+                await using var reqStream = await req.GetRequestStreamAsync();
+                await reqStream.WriteAsync(data);
+
+                var res = await req.GetResponseAsync() as HttpWebResponse;
+                await using var resStream = res!.GetResponseStream();
+                var reader = new StreamReader(resStream!, Encoding.UTF8);
+                results[i] = await reader.ReadToEndAsync();
+            }
+
+            while (true)
+            {
+                try
+                {
+                    var data = await util.ReceiveMsgAsync();
+                    var i = results.Count;
+                    results.Add("");
+                    var task = BrotliDec(data, i);
+                    tasks.Add(task);
+                }
+                catch (WebSocketUtils.WebSocketUtilException e)
+                {
+                    if (e.I != 0 || e.Type != WebSocketMessageType.Text ||
+                        Encoding.ASCII.GetString(e.Buf, 0, e.MsgSize) != "bye")
+                    {
+                        throw new ArchiveException("ArcaeaProberGet fail: Exit without bye");
+                    }
+
+                    await Task.WhenAll(tasks);
+                    return results;
+                }
+            }
+        }
+    }
+
+    internal class WebSocketUtils
+    {
+        private WebSocket Ws { get; }
+        private const int BufSize = 4095;
+
+        public WebSocketUtils(WebSocket ws)
+        {
+            Ws = ws;
+        }
+
+        public async Task SendTextAsync(string text)
+        {
+            await Ws.SendAsync(Encoding.UTF8.GetBytes(text), WebSocketMessageType.Text, true,
+                CancellationToken.None);
+        }
+
+        public async Task<byte[]> ReceiveMsgAsync(WebSocketMessageType type = WebSocketMessageType.Binary)
+        {
+            var buf = new byte[BufSize];
+            var i = 0;
+            while (true)
+            {
+                var slice = new ArraySegment<byte>(buf, i, BufSize - i);
+                var res = await Ws.ReceiveAsync(slice, CancellationToken.None);
+                if (res.MessageType != type)
+                {
+                    throw new WebSocketUtilException(buf, i, res.MessageType, res.Count);
+                }
+
+                i += res.Count;
+                if (res.EndOfMessage)
+                {
+                    break;
+                }
+            }
+
+            return buf.Take(i).ToArray();
+        }
+
+        public class WebSocketUtilException : Exception
+        {
+            public byte[] Buf { get; }
+            public int I { get; }
+            public WebSocketMessageType Type { get; }
+            public int MsgSize { get; }
+
+            public WebSocketUtilException(byte[] buf, int i, WebSocketMessageType type, int msgSize)
+            {
+                Buf = buf;
+                I = i;
+                Type = type;
+                MsgSize = msgSize;
+            }
+        }
+    }
+}
--- ScheduledArchive.cs
+++ ScheduledArchive.cs
@@ -1,5 +1,12 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using Microsoft.Azure.Cosmos.Table;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 
 namespace arcaea_archive_azfunc
 {
@@ -7,9 +14,92 @@ namespace arcaea_archive_azfunc
     public static class ScheduledArchive
     {
         [FunctionName("ScheduledArchive")]
-        public static void Run([TimerTrigger("0 0 * * * *")] TimerInfo timer, ILogger log)
+        // ReSharper disable once UnusedMember.Global
+        public static void Run([TimerTrigger("0 0 * * * *")]
+            // ReSharper disable once UnusedParameter.Global
+            TimerInfo timer, ILogger log)
         {
-            log.LogInformation("ScheduledArchive ok: date=");
+            var date = DateTime.UtcNow.ToString("s");
+            var dsn = Environment.GetEnvironmentVariable("COSMOS_CONN_CONFIG");
+            var account = CloudStorageAccount.Parse(dsn);
+            var client = account.CreateCloudTableClient(new TableClientConfiguration());
+            var table = client.GetTableReference("arcaea");
+
+            try
+            {
+                var task = ArcaeaProberService.Get();
+                task.Wait();
+                var results = task.Result;
+
+                var titleMsg = results[0];
+                results.RemoveAt(0);
+                var titles = JObject.Parse(titleMsg)["data"];
+
+                var userMsg = results[0];
+                results.RemoveAt(0);
+                var user = JObject.Parse(userMsg)["data"];
+
+                var songs = results.Select(m => (JArray) JObject.Parse(m)["data"])
+                    .Aggregate(new JArray(), (array, tokens) =>
+                    {
+                        foreach (var token in tokens)
+                        {
+                            array.Add(token);
+                        }
+
+                        return array;
+                    })
+                    .Select(s =>
+                    {
+                        var song = (JObject) s;
+                        var titleKey = (string) s["song_id"];
+                        if (titleKey == "worldvanquisher")
+                        {
+                            titleKey = "worldvanquier";
+                        }
+
+                        song.Add("title", titles[titleKey]);
+                        return song;
+                    });
+
+                var res = new JObject(new JProperty("user", user), new JProperty("songs", songs)).ToString();
+                using var output = new MemoryStream();
+                using var gzip = new GZipStream(output, CompressionMode.Compress);
+                using var input = new MemoryStream(Encoding.UTF8.GetBytes(res));
+                input.CopyTo(gzip);
+                gzip.Close();
+                var bin = output.ToArray();
+                var serveEntity = new ArcaeaRes("serve", "latest") {Res = bin};
+                var archiveEntity = new ArcaeaRes("archive", date) {Res = bin};
+                table.ExecuteAsync(TableOperation.InsertOrReplace(serveEntity)).Wait();
+                table.ExecuteAsync(TableOperation.InsertOrReplace(archiveEntity)).Wait();
+                log.LogInformation($"ScheduledArchive ok: date={date}");
+            }
+            catch (ArchiveException e)
+            {
+                log.LogError($"ScheduledArchive fail: {e.Reason}");
+            }
+        }
+    }
+
+    public class ArcaeaRes : TableEntity
+    {
+        public byte[] Res { get; set; }
+
+        public ArcaeaRes(string type, string date)
+        {
+            PartitionKey = type;
+            RowKey = date;
+        }
+    }
+
+    public class ArchiveException : Exception
+    {
+        public string Reason { get; }
+
+        public ArchiveException(string reason)
+        {
+            Reason = reason;
         }
     }
 }
